a109add9b94385d225eaf3e10550ffe119817ae7
Nutella585 <andrii.pylo.knm.2020@lpnu.ua>
2022-11-27 22:11:57 +0100

    Create main View Controller screen.

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func onAddTaskBtnPressed(_ sender: Any) {
+        print("GoalVC -> onAddTaskBtnPressed() ::: Button was pressed by user.")
+    }
+    
 }
 
