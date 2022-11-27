a510aab4ee07c407720916cab96a08a43e36af01
Nutella585 <andrii.pylo.knm.2020@lpnu.ua>
2022-11-28 00:03:24 +0100

    Create Table Cell.

---
+++ goalpost-app/Views/TaskCell.swift
@@ -0,0 +1,21 @@
+//
+//  TaskCell.swift
+//  goalpost-app
+//
+//  Created by Andrew Pylo on 27.11.2022.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet weak var descriptionLbl   : UILabel!
+    @IBOutlet weak var typeLbl          : UILabel!
+    @IBOutlet weak var progressLbl      : UILabel!
+    
+    func updateCell (desc: String, type: String, progress: Int) {
+        self.descriptionLbl.text = desc
+        self.typeLbl.text        = type
+        self.progressLbl.text    = String (describing: progress)
+    }
+}
