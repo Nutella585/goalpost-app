9dfd3e1afd36a087833884d1c96504759e1ea00c
Nutella585 <andrii.pylo.knm.2020@lpnu.ua>
2022-11-28 02:24:37 +0100

    Create View Controller for goal creation.

---
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -0,0 +1,34 @@
+//
+//  CreateGoalVC.swift
+//  goalpost-app
+//
+//  Created by Andrew Pylo on 28.11.2022.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var taskTextView     : UITextView!
+    @IBOutlet weak var taskLongTermBtn  : UIButton!
+    @IBOutlet weak var taskShortTermBtn : UIButton!
+    @IBOutlet weak var nextBtn          : UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
